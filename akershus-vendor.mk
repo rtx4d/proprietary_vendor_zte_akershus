1c4ff5849aa2f23bd31e88a9b695bcf56fe5c137
Bruno Martins <bgcngm@gmail.com>
2025-07-05 20:16:17 +0700

    akershus: Add IMS libs symlinks
    Change-Id: I32558148ce637542b8663a8d8aedb4fcddfdd092

--- akershus-vendor.mk
+++ akershus-vendor.mk
@@ -976,3 +976,7 @@ PRODUCT_PACKAGES += \
     android.hardware.neuralnetworks@1.3-service-qti \
     manifest_android.hardware.drm@1.3-service.widevine \
     vendor.qti.gnss@4.0-service
+
+PRODUCT_PACKAGES += \
+    libimscamera_jni_libimscamera_jni_symlink64 \
+    libimsmedia_jni_libimsmedia_jni_symlink64
